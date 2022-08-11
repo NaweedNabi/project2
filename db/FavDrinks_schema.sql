94331f709b2e9a20852a94183ca19def1039fd39
Jasmine Chamblin <jasminesimone.jc@gmail.com>
2022-08-10 19:39:00 -0700

    edited server js

---
+++ db/FavDrinks_schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Favdrinks_db;
+
+-- Create two new databases --
+DROP DATABASE IF EXISTS Favdrinks_db;
+CREATE DATABASE favdrinks_db;
+
+-- Use inventory_db --
+USE favdrinks_db;
+
+-- See database in use --
+SELECT DATABASE();
